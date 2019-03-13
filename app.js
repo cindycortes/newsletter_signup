3dde1f63d8b3ab140452668b866dd9e7a94d6608
Cindy Cortes <cindy.indins@gmail.com>
2019-03-12 17:47:29 -0700

    styling

--- app.js
+++ app.js
@@ -2,7 +2,17 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
 
-const app = express (); 
+const app = express(); 
+
+
+// Get Route
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + "/signup.html");
+})
+
+
+
 
 app.listen(3000, function() {
     console.log('Server is running on port 3000');
