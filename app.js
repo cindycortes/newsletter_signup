34b1fcd708fdc15c9b538dd6243cd35b3c5cfbd0
Cindy Cortes <cindy.indins@gmail.com>
2019-03-12 17:59:22 -0700

    post request in server

--- app.js
+++ app.js
@@ -2,16 +2,28 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
 
+
 const app = express(); 
 
 app.use(express.static('public')); // use the css styling for the static page
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 // Get Route
-
 app.get('/', function(req, res) {
     res.sendFile(__dirname + "/signup.html");
 })
 
+// Post Route
+app.post('/', function (req, res) {
+    
+    var firstName = req.body.fName;
+    var lastName = req.body.lName;
+    var email = req.body.email;
+
+    console.log(firstName, lastName, email);
+
+})
 
 
 
